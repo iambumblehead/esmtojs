52d01813b7934b7818ab3b8f484874f3fbe85345
chris <chris@bumblehead.com>
2024-05-07 00:26:59 -0700

    add tests

--- esmtojs.js
+++ esmtojs.js
@@ -2,7 +2,17 @@ const closuretpl = `let :name = (esmtojs => {
 :filestr
 })({})`
 
-export default (esmstr, name, spec, jsstr) => {
+export const buildImportReplaceRe = key => (
+  key = key.replace('/', '\\/').replace('.', '\\.'),
+  // '$' dollar sign used in immutable.js import variable name :(
+  // '/' comment slash used in comments inside import expression
+  new RegExp(
+    '(import|export)' + // dollar sign used in immutable.js
+      // `(?:[\\s.*]([\\w*{}\n\r\t, /$]+)[\\s*]from)?[\\s*](?:(["']${key}["']))`,
+      `(?:[\\s.*]([\\w*{}\n\r\t,"' /$]+)[\\s*]from)?[\\s*](?:(["']${key}["']))`,
+    'gm'))
+
+export default (esmstr, name, importmap, jsstr) => {
   jsstr = closuretpl
     .replace(/:name/, name)
     .replace(/:filestr/, esmstr)
@@ -10,6 +20,15 @@ export default (esmstr, name, spec, jsstr) => {
 
   jsstr = jsstr.replace(
     /export default/g, 'esmtojs =')
-  
+
+
+  jsstr = Object.keys(importmap).reduce((str, key) => {
+      const re = buildImportReplaceRe(key)
+
+    //console.log('re', re, String(str).match(re))
+      return str.replace(re, (match, g1, g2, g4) => (
+        match.replace(g4, `'${importmap[key]}'`)))
+    }, jsstr)
+
   return jsstr
 }
--- esmtojs.test.js
+++ esmtojs.test.js
@@ -1,6 +1,24 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
-import esmtojs from './esmtojs.js'
+import esmtojs, { buildImportReplaceRe } from './esmtojs.js'
+
+
+test('buildImportReplaceRe', () => {
+  const str = 'import { "stringname" as alias08 } from "module-name-08";'
+  const re = buildImportReplaceRe('module-name-08')
+
+  // const { default: stringname } = { default: re }
+  // console.log({ stringname })
+  // console.log({ re })
+  const strfin = str.replace(re, (match, g1, g2, g4) => (
+    // console.log({ g1, g2 }),
+    match.replace(g4, `'replaced'`)))
+
+  assert.strictEqual(
+    strfin, `import { "stringname" as alias08 } from 'replaced';`)
+})
+
+
 
 const esmBasic01 = `
 export default 'hello world'`
@@ -15,7 +33,67 @@ test('convert simple esm module', () => {
     esmtojs(esmBasic01, 'test01', {}), `
 let test01 = (esmtojs => {
 
-esmtojs = 'hello worlds'
+esmtojs = 'hello world'
+})({})
+`.slice(1, -1))
+
+  assert.strictEqual(
+    esmtojs(esmBasic02, 'test02', {}), `
+let test02 = (esmtojs => {
+
+esmtojs = Object.assign(domwh, {
+  window: domwhwindow
+})
+})({})
+`.slice(1, -1))  
+})
+
+const esmImport01 = `
+import defaultExport01 from "module-name-01";
+import * as name02 from "module-name-02";
+import { export03 } from "module-name-03";
+import { export04 as alias04 } from "module-name-04";
+import { default as alias05 } from "module-name-05";
+import { export061, export062 } from "module-name-06";
+import { export071, export072 as alias072, export073 } from "module-name-07";
+import { "stringname" as alias08 } from "module-name-08";
+import defaultExport09, { export091, export092 } from "module-name-09";
+import defaultExport10, * as name10 from "module-name-10";
+import "module-name-11";
+
+export default 'hello world'`
+
+test('convert simple esm imports', () => {
+  assert.strictEqual(
+    esmtojs(esmImport01, 'test03', {
+      'module-name-01': './local/test-module-name-01.js',
+      'module-name-02': './local/test-module-name-02.js',
+      'module-name-03': './local/test-module-name-03.js',
+      'module-name-04': './local/test-module-name-04.js',
+      'module-name-05': './local/test-module-name-05.js',
+      'module-name-06': './local/test-module-name-06.js',
+      'module-name-07': './local/test-module-name-07.js',
+      'module-name-08': './local/test-module-name-08.js',
+      'module-name-09': './local/test-module-name-09.js',
+      'module-name-10': './local/test-module-name-10.js',
+      'module-name-11': './local/test-module-name-11.js'
+    }), `
+let test03 = (esmtojs => {
+
+import defaultExport01 from './local/test-module-name-01.js';
+import * as name02 from './local/test-module-name-02.js';
+import { export03 } from './local/test-module-name-03.js';
+import { export04 as alias04 } from './local/test-module-name-04.js';
+import { default as alias05 } from './local/test-module-name-05.js';
+import { export061, export062 } from './local/test-module-name-06.js';
+import { export071, export072 as alias072, export073 } from './local/test-module-name-07.js';
+import { "stringname" as alias08 } from './local/test-module-name-08.js';
+import defaultExport09, { export091, export092 } from './local/test-module-name-09.js';
+import defaultExport10, * as name10 from './local/test-module-name-10.js';
+import './local/test-module-name-11.js';
+
+esmtojs = 'hello world'
 })({})
 `.slice(1, -1))
 })
+
